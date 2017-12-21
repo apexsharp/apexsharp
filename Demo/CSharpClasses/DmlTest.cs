b93e1f9f9695ccb1752304846f53a8dfa82c6486
Jay Janarthanan <jay@jayonsoftware.com>
2017-12-21 17:42:20 -0500

    But on Upsert

--- Demo/CSharpClasses/DmlTest.cs
+++ Demo/CSharpClasses/DmlTest.cs
@@ -10,9 +10,9 @@ namespace Demo.CSharpClasses
         public static void UpsertTest()
         {
             Contact contactNew = new Contact();
-            contactNew.LastName = "apexSharp";
-            contactNew.Email = "abc@abc.com";
-            //upsert contactNew;
+            contactNew.LastName = "apexSharp1111111";
+            contactNew.Email = "abc@abc.com111111111";
+            Soql.upsert(contactNew);
             System.debug(contactNew.Id);
             List<Contact> contacts = Soql.query<Contact>("SELECT Id, Email FROM Contact WHERE Id = :contactNew.Id", contactNew.Id);
             foreach (Contact c in contacts)
@@ -21,7 +21,7 @@ namespace Demo.CSharpClasses
                 c.Email = "new@new.com";
             }
 
-           // upsert contacts;
+            Soql.upsert(contactNew);
             contacts = Soql.query<Contact>("SELECT Id, Email FROM Contact WHERE Id = :contactNew.Id", contactNew.Id);
             foreach (Contact c in contacts)
             {
