3ff40a7d876cee26d601c17de2dd36a415f248a3
yallie <yallie@yandex.ru>
2018-02-14 11:43:35 +0300

    Added CreateTables and CreateAllTables methods to SqlLiteDemo.

--- Demo/SqlLiteDemo.cs
+++ Demo/SqlLiteDemo.cs
@@ -17,7 +17,9 @@ namespace Demo
             OrmLiteConfig.DialectProvider = SqliteDialect.Provider;
             using (IDbConnection db = "db.sqlite".OpenDbConnection())
             {
-                db.CreateTable<Account>(true);
+                //db.CreateTable<Account>(true);
+
+                CreateAllTables(db, true);
 
                 List<Account> accountList = SoqlApi.Query<Account>(5);
 
@@ -25,6 +27,33 @@ namespace Demo
 
                 var account = db.Select<Account>(x => x.Id == "0013600000Vm91zAAB").FirstOrDefault();
                 Console.WriteLine(account.Dump());
+            }
+        }
+
+        private static void CreateAllTables(IDbConnection db, bool overwrite)
+        {
+            CreateTables(db, null, overwrite);
+        }
+
+        private static void CreateTables(IDbConnection db, string[] classNames, bool overwrite)
+        {
+            // let's say that table types are all defined
+            // in the same assembly and namespace as Account type
+            var tableTypes =
+                from type in typeof(Account).Assembly.GetTypes()
+                where type.Namespace == typeof(Account).Namespace
+                select type;
+
+            // filter out the unneeded types as needed
+            if (classNames != null && classNames.Any())
+            {
+                tableTypes = tableTypes.Where(t => classNames.Contains(t.Name));
+            }
+
+            // create all tables
+            foreach (var type in tableTypes)
+            {
+                db.CreateTable(overwrite, type);
             }
         }
 
@@ -37,7 +66,7 @@ namespace Demo
                 var account = db.Select<Account>(x => x.Id == "0013600000Vm91zAAB").FirstOrDefault();
 
                 SoqlApi.Upsert(account);
-                
+
 
                 Console.WriteLine(account.Dump());
             }
